6aa8cf0bac9f8d14cf49dbddd37e5215204f0cc0
Brook Nega <brooknega21@gmail.com>
2022-09-21 17:40:17 -0400

    commit tree 42d19775787d33918116882c97c35064efbe919e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+function removeElement(){
+const main = document.querySelector("#main")
+
+main.remove()
+}
+
+removeElement()
+
+const main = document.querySelector("#main")
+const newHeader = document.createElement("h1")
+newHeader.id= ('victory')
+newHeader.textContent = "Brook is the champion"
+main.append(newHeader)
+
+
